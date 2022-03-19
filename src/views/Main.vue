2ba446ec06e2b8c699df85e856d4c347a17c80d5
leesanghooooon <sanghoooon0726@gmail.com>
2022-03-20 03:41:49 +0900

    누락파일커밋

---
+++ src/views/Main.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="body">
+    <Header />
+    <Body />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import Body from './Body.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    Header,
+    Body
+  },
+  data() {
+      return {
+          users: [],
+          numberOfUsers: 0
+      }
+  },
+  methods: {
+  },
+  mounted () {
+  }
+}
+</script>
